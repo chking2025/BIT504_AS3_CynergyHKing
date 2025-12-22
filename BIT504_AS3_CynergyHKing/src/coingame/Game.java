dd167535bdf9755219b76a80a9d0dd8129ebc15c
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-12-23 08:33:50 +1300

    Renamed class to 'Game'

---
+++ BIT504_AS3_CynergyHKing/src/coingame/Game.java
@@ -0,0 +1,52 @@
+package coingame;
+import javax.swing.*;
+
+/* Cynergy Huaki-King
+ * BIT504: Assessment 3
+ * 5009119
+ */
+
+public class Game extends JFrame {
+	
+	// DEFAULT CONSTRUCTOR
+	
+	public Game () {
+		
+		setTitle ("Coin Game");
+		setSize (800, 600);
+		setResizable (false);
+		add(new GamePanel());
+		setVisible (true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+	} // end of default constructor
+	
+	//--------------------------------------------------------------------------//
+	
+	// MAIN METHOD
+
+	public static void main(String[] args) {
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			
+			// creates game window
+			
+			public void run () {
+				
+				new Game();
+				
+			} // end of run method
+		});
+		
+		
+
+	} // end of main method
+	
+	//--------------------------------------------------------------------------//
+	
+	// METHODS
+	
+
+
+} // end of Game class
--- BIT504_AS3_CynergyHKing/src/coingame/GamePanel.java
+++ BIT504_AS3_CynergyHKing/src/coingame/GamePanel.java
@@ -1,5 +1,65 @@
 package coingame;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class GamePanel {
+/* Cynergy Huaki-King
+ * BIT504: Assessment 3
+ * 5009119
+ */
 
-}
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
+	
+	// CLASS VARIABLES
+	
+	private final static Color BACKGROUND_COLOUR = Color.GRAY;
+	
+	//--------------------------------------------------------------------------//
+	
+	// DEFAULT CONSTRUCTOR
+	
+	public GamePanel () {
+		
+		setBackground(BACKGROUND_COLOUR);
+		
+		
+	} // end of default constructor
+	
+	//--------------------------------------------------------------------------//
+	
+	// METHODS
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	} // end of keyTyped method
+	
+	//--------------------------------------------------------------------------//
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		
+	} // end of keyPressed method
+	
+	//--------------------------------------------------------------------------//
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		
+	} // end of keyReleased method
+	
+	//--------------------------------------------------------------------------//
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	} // end of actionPerformed method
+
+	//--------------------------------------------------------------------------//
+	
+	
+} // end of GamePanel class
