b83713580c5fc85f9df46095a0da588288ab67dd
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-12-24 07:52:13 +1300

    Added variables and a constructor

--- BIT504_AS3_CynergyHKing/src/coingame/Enemy.java
+++ BIT504_AS3_CynergyHKing/src/coingame/Enemy.java
@@ -1,5 +1,56 @@
 package coingame;
 
-public class Enemy {
+import java.awt.Color;
 
-}
+/* Cynergy Huaki-King
+ * BIT504: Assessment 3
+ * 5009119
+ */
+
+public class Enemy extends Sprite {
+
+	// CLASS VARIABLES
+	
+	private static final int ENEMY_WIDTH = 30;
+	private static final int ENEMY_HEIGHT = 30;
+	private static final Color ENEMY_COLOUR = Color.orange;
+	
+	//--------------------------------------------------------------------------//
+	
+	// ENEMY CONSTRUCTOR
+	
+	public Enemy (int panelWidth, int panelHeight) {
+		
+		setWidth(ENEMY_WIDTH);
+		setHeight(ENEMY_HEIGHT);
+		setColour(ENEMY_COLOUR);
+		
+		
+	} // end of Enemy constructor
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // end of Enemy class
--- BIT504_AS3_CynergyHKing/src/coingame/Player.java
+++ BIT504_AS3_CynergyHKing/src/coingame/Player.java
@@ -1,5 +1,53 @@
 package coingame;
+import java.awt.*;
 
-public class Player {
+/* Cynergy Huaki-King
+ * BIT504: Assessment 3
+ * 5009119
+ */
 
-}
+public class Player extends Sprite {
+
+	// CLASS VARIABLES
+	
+	private static final int PLAYER_WIDTH = 30;
+	private static final int PLAYER_HEIGHT = 30;
+	private static final Color PLAYER_COLOUR = Color.green;
+	
+	//--------------------------------------------------------------------------//
+	
+	// PLAYER CONSTRUCTOR
+	
+	public Player (int panelWidth, int panelHeight) {
+		
+		setWidth(PLAYER_WIDTH);
+		setHeight(PLAYER_HEIGHT);
+		setColour(PLAYER_COLOUR);
+		
+		
+	} // end of Player constructor
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // end of Player class
