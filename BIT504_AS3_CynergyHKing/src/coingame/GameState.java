c0f61dd62781fb1f59fab0e2f2b7a03944ddbea9
BIT504 <cynergykinghuaki@myop.ac.nz>
2025-12-24 08:47:30 +1300

    Removed object from constructor

--- BIT504_AS3_CynergyHKing/src/coingame/GameState.java
+++ BIT504_AS3_CynergyHKing/src/coingame/GameState.java
@@ -1,5 +1,10 @@
 package coingame;
 
+/* Cynergy Huaki-King
+ * BIT504: Assessment 3
+ * 5009119
+ */
+
 public enum GameState {
 
 	INITIALIZING,
--- BIT504_AS3_CynergyHKing/src/coingame/Player.java
+++ BIT504_AS3_CynergyHKing/src/coingame/Player.java
@@ -13,6 +13,7 @@ public class Player extends Sprite {
 	private static final int PLAYER_WIDTH = 30;
 	private static final int PLAYER_HEIGHT = 30;
 	private static final Color PLAYER_COLOUR = Color.green;
+	private static final int DISTANCE_FROM_EDGE = 40;
 	
 	//--------------------------------------------------------------------------//
 	
@@ -23,6 +24,10 @@ public class Player extends Sprite {
 		setWidth(PLAYER_WIDTH);
 		setHeight(PLAYER_HEIGHT);
 		setColour(PLAYER_COLOUR);
+		
+		int xPosition = panelWidth - DISTANCE_FROM_EDGE - getWidth();
+		setInitialPosition (xPosition, panelHeight / 2 - (getHeight() / 2));
+		resetToInitialPosition();
 		
 		
 	} // end of Player constructor
